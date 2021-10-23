85620ebc4d72938860e443926801916275120a4b
Stanislav Lebedev <lebedevsf239@gmail.com>
2021-10-23 15:10:22 +0300

    upgrage generation. Add labels saving

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 1) Ставим блендер
-2) Открываем data/generate.py блендером, запускаем
-3) Смотрим как можно запускать на расчет модельку. Пишем циклы по желанию
+2) ```
+   blender -b --python generate.py -- <MODELS_DIR_PATH> <MODELS_NUMBER>
+   ```
+   , где <MODELS_DIR_PATH>--путь к директории сохранения моделек, <MODELS_NUMBER> -- количество моделек
+3) Заглядываем в [гайд](https://github.com/Stasiche/prod_stories_hw4_SPBSTU/blob/main/guide.ipynb)
--- data/generate.py
+++ data/generate.py
@@ -4,6 +4,8 @@ import uuid
 import math
 import datetime
 import os
+from argparse import ArgumentParser
+import sys
 
 primitive_constructors = [
     bpy.ops.mesh.primitive_cube_add,
@@ -12,25 +14,45 @@ primitive_constructors = [
     bpy.ops.mesh.primitive_cone_add,
     bpy.ops.mesh.primitive_torus_add,
 ]
+dpi = 2 * math.pi
 
-dpi = 2*math.pi
 
-model_folder = "/tmp/models"
-if not os.path.exists(model_folder):
-    os.mkdir(model_folder)
+def main(model_folder: str, k: int):
+    if not os.path.exists(model_folder):
+        os.mkdir(model_folder)
 
-for ctor in random.choices(primitive_constructors, k=10):
-    ctor()
-    bpy.context.active_object.select_set(True)
-    transform_seed = datetime.datetime.now().microsecond % 100
-    print(transform_seed)
-    bpy.ops.object.randomize_transform(
-        random_seed=transform_seed, 
-        loc=(10, 10, 10),
-        rot=(dpi, dpi, dpi),
-        scale=(10, 10, 10),
-    )
-    bpy.ops.export_mesh.stl(
-        filepath=os.path.join(model_folder, f"{uuid.uuid4()}.stl"), 
-    )
-    bpy.ops.object.delete()
+    with open(f"{model_folder}/labels.txt", 'w') as f:
+        for ctor in random.choices(primitive_constructors, k=k):
+            ctor()
+            bpy.context.active_object.select_set(True)
+            transform_seed = datetime.datetime.now().microsecond % 100
+            print(transform_seed)
+            bpy.ops.object.randomize_transform(
+                random_seed=transform_seed,
+                loc=(10, 10, 10),
+                rot=(dpi, dpi, dpi),
+                scale=(10, 10, 10),
+            )
+            fn = f"{uuid.uuid4()}.stl"
+            bpy.ops.export_mesh.stl(
+                filepath=os.path.join(model_folder, fn),
+            )
+            f.write(f'{fn}, {bpy.context.active_object.name}\n')
+            bpy.ops.object.delete()
+
+
+if __name__ == "__main__":
+    # __arg_parser = ArgumentParser()
+    # __arg_parser.add_argument('--models', type=str, help='Path to directory to save stl models')
+    # __arg_parser.add_argument('-mn', type=int, default=100, help='Number of models to generate')
+    #
+    # __args = __arg_parser.parse_args()
+    # main(__args.models, __args.k)
+
+    argv = sys.argv
+    argv = argv[argv.index("--") + 1:]
+    print(argv)
+    if len(argv) == 2:
+        main(argv[0], int(argv[1]))
+    else:
+        main(argv[0], 100)
